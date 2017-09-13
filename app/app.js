3d84002f8a128fc94c37000348b245a29e820a08
Brian Lee <brianjleeofcl@gmail.com>
2017-09-12 23:45:31 -0700

    Add server with reddit 0Auth

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
 const app = express()
+const axios = require('axios');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 app.disable('x-powered-by');
 
@@ -9,7 +12,30 @@ app.use(
   require('body-parser').json()
 )
 
-// app.use('/api',require('./api/api'))
+app.get('/start', (req, res) => {
+  // makes sure only requests with secret gets redirected; this prevents any random user from accessing the app.
+  if (req.query.secret === process.env.SECRET_INIT) res.redirect(302, `https://www.reddit.com/api/v1/authorize?client_id=${process.env.REDDIT_CLIENT_ID}&response_type=code&state=${process.env.REDDIT_STATE_STRING}&redirect_uri=https://tbtl-showfeed.herokuapp.com/redirect&duration=permanent&scope=submit`);
+  else res.sendStatus(403);
+})
+
+app.get('/redirect', (req, res) => {
+  const { code, state } = req.query;
+  if (state !== process.env.REDDIT_STATE_STRING) return res.sendStatus(403);
+  else {
+    res.sendStatus(200);
+    axios.post(
+      'https://www.reddit.com/api/v1/access_token', 
+      `grant_type=authorization_code&code=${code}&redirect_uri=https://tbtl-showfeed.herokuapp.com/redirect`, 
+      { auth: {
+        username: process.env.REDDIT_CLIENT_ID, password: process.env.REDDIT_CLIEN_TSECRET
+      }
+    }).then(({data}) => {
+      console.log(require('util').inspect(data));
+    });
+  }
+})
+
+// app.use('/api', require('./api/api'));
 
 // app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
