a89352bc8f1383546bc5c17d51ac722b887436ad
Brian Lee <brianjleeofcl@gmail.com>
2017-09-14 01:29:14 -0700

    refresh test

--- app/api/routes/new-post.js
+++ app/api/routes/new-post.js
@@ -2,9 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 const knex = require('../../knex');
+const axios = require('axios');
+
+const refreshToken = function(req, res) {
+  knex('reddit_users').where('user_name', 'tbtl_showfeed').select('refresh_token').then(([refreshToken]) => {
+    return axios.post('https://www.reddit.com/api/v1/access_token', {
+      data: `grant_type=refresh_token&refresh_token=${refreshToken}`,
+      auth: {
+        username: process.env.REDDIT_CLIENT_ID, 
+        password: process.env.REDDIT_CLIENT_SECRET
+      }
+    })
+  }).then(({data}) => {
+    console.log(require('util').inspect(data))
+  })
+}
 
 router.post('/', (req, res) => {
   const { title, url, description } = req.body;
+
+
+
+});
+
+router.post('/test', refreshToken, (req, res) => {
+  res.sendStatus(200);
 });
 
 module.exports = router;
